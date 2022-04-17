71a580214a0f9d990bd28f9ff7e336a30fc45f61
ahmedsat <ahmedsat303@gmail.com>
2022-04-17 22:19:28 +0000

    reset

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyHomePage extends StatelessWidget {
       ),
       body: Center(
         child: Text(
-          'Hello, World! ',
+          'Hello, World!',
         ),
       ),
     );
