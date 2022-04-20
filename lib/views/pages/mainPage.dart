f9b7414c3fb32fbb81f52e741112e5f494844fca
FlutLab.io <>
2022-04-20 04:20:23 +0000

    Autosave

--- lib/views/pages/mainPage.dart
+++ lib/views/pages/mainPage.dart
@@ -105,7 +105,6 @@ class _MainPageState extends State<MainPage> {
           ),
           Container(
             margin: const EdgeInsets.all(10),
-            height: 200,
             child: GridView.builder(
               gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                 crossAxisSpacing: 10,
