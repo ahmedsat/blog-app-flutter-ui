939104a9104edb3bf1fbdf617819b4a56c4ddf68
FlutLab.io <>
2022-04-20 03:59:43 +0000

    Autosave

--- lib/views/pages/mainPage.dart
+++ lib/views/pages/mainPage.dart
@@ -142,7 +142,7 @@ class _MainPageState extends State<MainPage> {
               gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                 crossAxisSpacing: 10,
                 mainAxisSpacing: 10,
-                childAspectRatio: .90,
+                childAspectRatio: 2,
                 crossAxisCount: 2,
               ),
               scrollDirection: Axis.horizontal,
