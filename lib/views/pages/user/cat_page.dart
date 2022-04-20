d0c5468e75c8baaaf8011e2e3c3e2129835484a1
ahmedsat <ahmedsat303@gmail.com>
2022-04-20 21:50:11 +0000

    Category page ui done

--- lib/views/pages/user/cat_page.dart
+++ lib/views/pages/user/cat_page.dart
@@ -1,3 +1,9 @@
+import 'package:elmhanes/views/widgets/custom_app_bar.dart';
+import 'package:elmhanes/views/widgets/custom_drawer.dart';
+import 'package:elmhanes/views/widgets/extra_small_content_card_ric.dart';
+import 'package:elmhanes/views/widgets/login_card.dart';
+import 'package:elmhanes/views/widgets/my_header.dart';
+import 'package:elmhanes/views/widgets/social_icon.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -15,6 +21,7 @@ class _CategoryPageState extends State<CategoryPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: const CustomAppBar(),
       body: SingleChildScrollView(
         controller: controller,
         child: Column(
@@ -22,142 +29,157 @@ class _CategoryPageState extends State<CategoryPage> {
           children: <Widget>[
             MyHeader(
               image: "assets/icons/barbecue.svg",
-              textTop: "Order and",
-              textBottom: "Get to door steps",
+              textTop: "عنوان القسم",
+              textBottom:
+                  "لا أحد يحب الألم بذاته، يسعى ورائه أو يبتغيه، ببساطة لأنه الألم... ",
               offset: offset,
             ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  loginCard(),
-                  SizedBox(height: 40),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Row(
-                        children: <Widget>[
-                          SizedBox(
-                            width: 12.0,
-                          ),
-                          GestureDetector(
-                            onTap: _radio,
-                            child: radioButton(_isSelected),
-                          ),
-                          SizedBox(
-                            width: 8.0,
-                          ),
-                          Text("Remember me",
-                              style: TextStyle(
-                                  fontSize: 12, fontFamily: "Poppins-Medium"))
-                        ],
-                      ),
-                      InkWell(
-                        child: Container(
-                          width: 330,
-                          height: 100,
-                          decoration: BoxDecoration(
-                              color: Color(0xFF3382CC),
-                              borderRadius: BorderRadius.circular(6.0),
-                              boxShadow: [
-                                BoxShadow(
-                                    color: Color(0xFF4056C6).withOpacity(.15),
-                                    offset: Offset(0.0, 8.0),
-                                    blurRadius: 8.0)
-                              ]),
-                          child: Material(
-                            color: Colors.transparent,
-                            child: InkWell(
-                              onTap: () {},
-                              child: Center(
-                                child: Text("SIGNIN",
-                                    style: TextStyle(
-                                        color: Colors.white,
-                                        fontFamily: "Poppins-Bold",
-                                        fontSize: 18,
-                                        letterSpacing: 1.0)),
-                              ),
-                            ),
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
-                  SizedBox(
-                    height: 40,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      horizontalLine(),
-                      Text("Social Login",
-                          style: TextStyle(
-                              fontSize: 16.0, fontFamily: "Poppins-Medium")),
-                      horizontalLine()
-                    ],
-                  ),
-                  SizedBox(
-                    height: 40,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      SocialIcon(
-                        color: Color(0xFF102397),
-                        iconData: IconData(0xe901, fontFamily: "CustomIcons"),
-                        onPressed: () {},
-                      ),
-                      SocialIcon(
-                        color: Color(0xFFff4f38),
-                        iconData: IconData(0xe902, fontFamily: "CustomIcons"),
-                        onPressed: () {},
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text(
-                        "New User? ",
-                        style: TextStyle(fontFamily: "Poppins-Medium"),
-                      ),
-                      InkWell(
-                        onTap: () {},
-                        child: Text("SignUp",
-                            style: TextStyle(
-                                color: Color(0xFF5d74e3),
-                                fontFamily: "Poppins-Bold")),
-                      )
-                    ],
-                  )
-                ],
-              ),
-            )
+            ExtraSmallContentCardRic(
+              icon: Icons.topic,
+              title: 'test title',
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            ExtraSmallContentCardRic(
+              icon: Icons.topic,
+              title: 'test title',
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            ExtraSmallContentCardRic(
+              icon: Icons.topic,
+              title: 'test title',
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            ExtraSmallContentCardRic(
+              icon: Icons.topic,
+              title: 'test title',
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            ExtraSmallContentCardRic(
+              icon: Icons.topic,
+              title: 'test title',
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            ExtraSmallContentCardRic(
+              icon: Icons.topic,
+              title: 'test title',
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            ExtraSmallContentCardRic(
+              icon: Icons.topic,
+              title: 'test title',
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            ExtraSmallContentCardRic(
+              icon: Icons.topic,
+              title: 'test title',
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            ExtraSmallContentCardRic(
+              icon: Icons.topic,
+              title: 'test title',
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            ExtraSmallContentCardRic(
+              icon: Icons.topic,
+              title: 'test title',
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            ExtraSmallContentCardRic(
+              icon: Icons.topic,
+              title: 'test title',
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            ExtraSmallContentCardRic(
+              icon: Icons.topic,
+              title: 'test title',
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            ExtraSmallContentCardRic(
+              icon: Icons.topic,
+              title: 'test title',
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            ExtraSmallContentCardRic(
+              icon: Icons.topic,
+              title: 'test title',
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            ExtraSmallContentCardRic(
+              icon: Icons.topic,
+              title: 'test title',
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            ExtraSmallContentCardRic(
+              icon: Icons.topic,
+              title: 'test title',
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            ExtraSmallContentCardRic(
+              icon: Icons.topic,
+              title: 'test title',
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            ExtraSmallContentCardRic(
+              icon: Icons.topic,
+              title: 'test title',
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            ExtraSmallContentCardRic(
+              icon: Icons.topic,
+              title: 'test title',
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            ExtraSmallContentCardRic(
+              icon: Icons.topic,
+              title: 'test title',
+            ),
+            const SizedBox(
+              height: 5,
+            ),
           ],
         ),
       ),
+      drawer: const CustomDrawer(),
     );
   }
-
-  @override
-  void dispose() {
-    // TODO: implement dispose
-    controller.dispose();
-    super.dispose();
-  }
-
-  Widget horizontalLine() => Padding(
-        padding: EdgeInsets.symmetric(horizontal: 16.0),
-        child: Container(
-          width: 120,
-          height: 1.0,
-          color: Colors.black26.withOpacity(.2),
-        ),
-      );
 
   @override
   void initState() {
@@ -170,223 +192,5 @@ class _CategoryPageState extends State<CategoryPage> {
     setState(() {
       offset = (controller.hasClients) ? controller.offset : 0;
     });
-  }
-
-  Widget radioButton(bool isSelected) => Container(
-        width: 16.0,
-        height: 16.0,
-        padding: EdgeInsets.all(2.0),
-        decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            border: Border.all(width: 2.0, color: Colors.black)),
-        child: isSelected
-            ? Container(
-                width: double.infinity,
-                height: double.infinity,
-                decoration:
-                    BoxDecoration(shape: BoxShape.circle, color: Colors.black),
-              )
-            : Container(),
-      );
-
-  void _radio() {
-    setState(() {
-      _isSelected = !_isSelected;
-    });
-  }
-}
-
-class MyHeader extends StatefulWidget {
-  final String image;
-  final String textTop;
-  final String textBottom;
-  final double offset;
-  const MyHeader({
-    Key? key,
-    required this.image,
-    required this.textTop,
-    required this.textBottom,
-    required this.offset,
-  }) : super(key: key);
-
-  @override
-  _MyHeaderState createState() => _MyHeaderState();
-}
-
-class _MyHeaderState extends State<MyHeader> {
-  @override
-  Widget build(BuildContext context) {
-    return ClipPath(
-      clipper: MyClipper(),
-      child: Container(
-        padding: EdgeInsets.only(left: 40, top: 50, right: 20),
-        height: 350,
-        width: double.infinity,
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topRight,
-            end: Alignment.bottomLeft,
-            colors: [
-              Color(0xFF3383CD),
-              Color(0xFF11249F),
-            ],
-          ),
-          image: DecorationImage(
-            image: AssetImage("assets/images/virus.png"),
-          ),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: <Widget>[
-            SizedBox(height: 30),
-            Expanded(
-              child: Stack(
-                children: <Widget>[
-                  Positioned(
-                    top: (widget.offset < 0) ? 0 : widget.offset,
-                    child: SvgPicture.asset(
-                      widget.image,
-                      width: 270,
-                      fit: BoxFit.fitWidth,
-                      alignment: Alignment.bottomLeft,
-                    ),
-                  ),
-                  Positioned(
-                    top: 20 - widget.offset / 2,
-                    left: 140,
-                    child: Text(
-                      "${widget.textTop} \n${widget.textBottom}",
-                      style: TextStyle(
-                        fontSize: 22,
-                        fontWeight: FontWeight.w600,
-                      ).copyWith(
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                  Container(), // I dont know why it can't work without container
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class MyClipper extends CustomClipper<Path> {
-  @override
-  Path getClip(Size size) {
-    var path = Path();
-    path.lineTo(0, size.height - 80);
-    path.quadraticBezierTo(
-        size.width / 2, size.height, size.width, size.height - 80);
-    path.lineTo(size.width, 0);
-    path.close();
-    return path;
-  }
-
-  @override
-  bool shouldReclip(CustomClipper<Path> oldClipper) {
-    return false;
-  }
-}
-
-class loginCard extends StatelessWidget {
-  bool passwordInvisible = true;
-
-  @override
-  Widget build(BuildContext context) {
-    return new Container(
-      width: double.infinity,
-      padding: EdgeInsets.only(bottom: 1),
-      decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(8.0),
-          boxShadow: [
-            BoxShadow(
-                color: Colors.black12,
-                offset: Offset(0.0, 15.0),
-                blurRadius: 15.0),
-            BoxShadow(
-                color: Colors.black12,
-                offset: Offset(0.0, -10.0),
-                blurRadius: 10.0),
-          ]),
-      child: Padding(
-        padding: EdgeInsets.only(left: 16.0, right: 16.0, top: 16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text("Login",
-                style: TextStyle(
-                    fontSize: 45,
-                    fontFamily: "Poppins-Bold",
-                    letterSpacing: .6)),
-            SizedBox(
-              height: 30,
-            ),
-            Text("Username",
-                style: TextStyle(fontFamily: "Poppins", fontSize: 26)),
-            TextField(
-              decoration: InputDecoration(
-                  hintText: "eg: chromicle",
-                  hintStyle: TextStyle(color: Colors.grey, fontSize: 12.0)),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            Text("PassWord",
-                style: TextStyle(fontFamily: "Poppins", fontSize: 26)),
-            TextFormField(
-              obscureText: true,
-              decoration: InputDecoration(
-                  suffixIcon: IconButton(
-                    icon: Icon(
-                      passwordInvisible
-                          ? Icons.visibility_off
-                          : Icons.visibility,
-                      color: Colors.black,
-                    ),
-                    onPressed: () {},
-                  ),
-                  hintText: "**********",
-                  hintStyle: TextStyle(color: Colors.grey, fontSize: 12.0)),
-            ),
-            SizedBox(
-              height: 35,
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class SocialIcon extends StatelessWidget {
-  final Color color;
-  final IconData iconData;
-  final Function onPressed;
-  SocialIcon({
-    required this.color,
-    required this.iconData,
-    required this.onPressed,
-  });
-  @override
-  Widget build(BuildContext context) {
-    return new Padding(
-      padding: EdgeInsets.only(left: 14.0),
-      child: Container(
-        width: 45.0,
-        height: 45.0,
-        decoration: BoxDecoration(shape: BoxShape.circle, color: color),
-        child: RawMaterialButton(
-          shape: CircleBorder(),
-          onPressed: () {},
-          child: Icon(iconData, color: Colors.white),
-        ),
-      ),
-    );
   }
 }
