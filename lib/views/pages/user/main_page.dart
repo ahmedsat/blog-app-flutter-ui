a7173e5e9933685aa6cf8a9c025ba6c5c84e40c1
FlutLab.io <>
2022-04-21 06:49:00 +0000

    Autosave

--- lib/views/pages/user/main_page.dart
+++ lib/views/pages/user/main_page.dart
@@ -79,7 +79,7 @@ class RowCard extends StatelessWidget {
       ),
       width: MediaQuery.of(context).size.width,
       child: Text(
-        'data',
+        """data""",
         style: TextStyle(fontSize: 20),
       ),
     );
