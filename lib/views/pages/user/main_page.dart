08a2aea41a3c33b358f85c08cf9a79f251627f5e
FlutLab.io <>
2022-04-21 05:28:30 +0000

    Autosave

--- lib/views/pages/user/main_page.dart
+++ lib/views/pages/user/main_page.dart
@@ -30,7 +30,7 @@ class _MainPageState extends State<MainPage> {
         ),
         child: GFAccordion(
           title: 'GF Accordion',
-          content: 'GetFlutter is an open source library that comes with  pre-build 1000+ UI components.',
+          // content: 'GetFlutter is an open source library that comes with  pre-build 1000+ UI components.',
           contentChild: ListView.builder(
             itemCount: items.length,
             itemBuilder: (context, i) {
