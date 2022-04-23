f8e03ced5ad7fe2923c6bcde404d22d5ddd1b4e5
FlutLab.io <>
2022-04-23 16:14:39 +0000

    Autosave

--- lib/views/widgets/custom_drawer.dart
+++ lib/views/widgets/custom_drawer.dart
@@ -1,4 +1,3 @@
-import 'package:elmhandes/controllers/auth_controller.dart';
 import 'package:elmhandes/views/services/custom_snackbar.dart';
 import 'package:flutter/material.dart';
 import 'package:get/route_manager.dart';
@@ -56,7 +55,11 @@ class CustomDrawer extends StatelessWidget {
               title: const Text('LogOut'),
               trailing: const Icon(Icons.logout),
               onTap: () {
-                AuthController.instance.logout();
+                CustomSnackbar(
+                  title: 'Log Out',
+                  message: 'Done',
+                  icon: Icons.logout,
+                );
               },
             ),
           ),
