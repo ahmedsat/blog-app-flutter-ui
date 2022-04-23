9775b7156ea940a14daf468f5b5e994367a0ad53
FlutLab.io <>
2022-04-23 12:59:25 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp().then(
     (value) {
-      Get.put(AuthController.instance);
+      Get.put(AuthController());
     },
     onError: (e) => print(e),
   );
