7de1ded142954915c3dec4b8ce51bbb9d6a9425f
FlutLab.io <>
2022-04-23 12:06:53 +0000

    Autosave

---
+++ lib/controllers/auth_controller.dart
@@ -0,0 +1,13 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get/get.dart';
+
+class AuthController extends GetxController {
+  static AuthController instance = Get.find();
+  late Rx<User?> _user;
+  FirebaseAuth auth = FirebaseAuth.instance;
+  @override
+  void onReady() {
+    super.onReady();
+    _user = Rx<User?>(auth.currentUser);
+  }
+}
