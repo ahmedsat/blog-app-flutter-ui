b763eda0b405574da8f2aaabab5c875e659c1fea
FlutLab.io <>
2022-04-24 12:32:40 +0000

    Autosave

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -48,9 +48,7 @@ class AuthController extends GetxController {
   _setMembership(User user) async {
     var usersRef = FirebaseFirestore.instance.collection(usersCollection);
     var snapshot = await usersRef.where('id', isEqualTo: auth.currentUser.uid).get();
-    print(snapshot.docs.first.data());
-
-    _membership = 0;
+    _membership = snapshot.docs.first.data()[membership];
   }
 
   void createUser(String email, password) async {
