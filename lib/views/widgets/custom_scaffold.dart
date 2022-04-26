cdb923b9f6b9342442794dfa291e7bb32fe261ec
FlutLab.io <>
2022-04-26 08:27:06 +0000

    Autosave

--- lib/views/widgets/custom_scaffold.dart
+++ lib/views/widgets/custom_scaffold.dart
@@ -11,9 +11,9 @@ class CustomScaffold extends StatelessWidget {
     return Directionality(
       textDirection: TextDirection.rtl,
       child: Scaffold(
-        appBar: const CustomAppBar(),
+        // appBar: const CustomAppBar(),
         body: body,
-        drawer: const CustomDrawer(),
+        // drawer: const CustomDrawer(),
       ),
     );
   }
