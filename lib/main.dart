8d58d37687a49182fe8d7364ad27b550d04ea4aa
FlutLab.io <>
2022-04-27 08:05:47 +0000

    Switching to branch test-2

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:get/get.dart';
 
 Future<void> main() async {
+  //
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp().then(
     (value) {
